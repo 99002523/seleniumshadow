8d0448490ad155f30f7be33144e6fd242518450a
99002523 <71481081+99002523@users.noreply.github.com>
2020-12-01 13:00:33 +0530

    Update hello.py

--- latest/outlook1/hello.py
+++ latest/outlook1/hello.py
@@ -1,59 +1,68 @@
-import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+import argparse
+import imutils
+import glob
+import cv2
 import pyautogui
-
 
 def Avg(a,b):
     c=0
     sum=[]
-    print(len(a))
     for i in range(len(a)):
         c=a[i]+b[i]
         c=int(c/2)
         sum.append(c)
     #sum[0]=sum[0]+439
-    #sum[1]=sum[1]+285
+    sum[1]=sum[1]+60
+    print(sum)
     return sum
 
-
 def Apr():
-    img = cv2.imread('D:\main\guru.png',0)
-    img2 = img.copy()
-    template = cv2.imread('D:\main\selenium.png',0)
-    w, h = template.shape[::-1]
 
-    # All the 6 methods for comparison in a list
-    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR','cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+        template = cv2.imread("D:\multi\images\logo.png")
+        template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        template = cv2.Canny(template, 50, 200)
+        (tH, tW) = template.shape[:2]
+        #cv2.imshow("Template", template)
 
-    for meth in methods:
-        img = img2.copy()
-        method = eval(meth)
+        # loop over the images to find the template in
+        for imagePath in glob.glob("D:\main" + "/*.png"):
+                # load the image, convert it to grayscale, and initialize the
+                # bookkeeping variable to keep track of the matched region
+                image = cv2.imread(imagePath)
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                found = None
+                # loop over the scales of the image
+                for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+                        # resize the image according to the scale, and keep track
+                        # of the ratio of the resizing
+                        resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
+                        r = gray.shape[1] / float(resized.shape[1])
+                        # if the resized image is smaller than the template, then break
+                        # from the loop
+                        if resized.shape[0] < tH or resized.shape[1] < tW:
+                                break
+        # detect edges in the resized, grayscale image and apply template
+                        # matching to find the template in the image
+                        edged = cv2.Canny(resized, 50, 200)
+                        result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+                        (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
 
-        # Apply template Matching
-        res = cv2.matchTemplate(img,template,method)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+                        if found is None or maxVal > found[0]:
+                                found = (maxVal, maxLoc, r)
 
-        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-            top_left = min_loc
-        else:
-            top_left = max_loc
-        bottom_right = (top_left[0] + w, top_left[1] + h)
+                (_, maxLoc, r) = found
+                (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+                (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
 
-        cv2.rectangle(img,top_left, bottom_right, 255, 2)
+                cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+                #cv2.imshow("Image", image)
+                cv2.waitKey(0)
+                X=(startX, startY)
+                Y=(endX, endY)
+                avg=Avg(X,Y)
+        return avg
 
-        plt.subplot(121),plt.imshow(res,cmap = 'gray')
-        plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-        plt.subplot(122),plt.imshow(img,cmap = 'gray')
-        plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-        plt.suptitle(meth)
-
-        # plt.show()
-    avg=Avg(top_left,bottom_right)
-    return top_left,bottom_right
-
-
-def coordinates_click(locator,a,b):
-    
+def coordinates_click(a,b):
     pyautogui.click(a,b) 
+
